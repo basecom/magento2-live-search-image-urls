51d87a6969a960d5049a9103ed0e4bb704f364d9
Finn Reinhardt <f.reinhardt@basecom.de>
2025-03-25 14:28:35 +0100

    Update ModuleConfig.php to retrieve new configurations

--- System/ModuleConfig.php
+++ System/ModuleConfig.php
@@ -3,15 +3,17 @@ declare(strict_types=1);
 
 namespace Basecom\LiveSearchImageUrls\System;
 
+use Basecom\LiveSearchImageUrls\Model\Config\Source\SelectedImageTypes;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 
 class ModuleConfig
 {
-    private const XML_PATH_RESIZE_MODE = 'storefront_features/image_dimensions/size_mode';
-    private const XML_PATH_RESIZE_IMAGE_ID = 'storefront_features/image_dimensions/image_id';
-    private const XML_PATH_RESIZE_HEIGHT = 'storefront_features/image_dimensions/resize_height';
-    private const XML_PATH_RESIZE_WIDTH = 'storefront_features/image_dimensions/resize_width';
+    private const XML_PATH_ACTIVE_IMAGE_TYPES = 'storefront_features/resize_images/image_types';
+    private const XML_PATH_RESIZE_MODE = 'storefront_features/resize_images/resize_mode';
+    private const XML_PATH_RESIZE_IMAGE_ID = 'storefront_features/resize_images/%s/image_id';
+    private const XML_PATH_RESIZE_HEIGHT = 'storefront_features/resize_images/%s/resize_height';
+    private const XML_PATH_RESIZE_WIDTH = 'storefront_features/resize_images/%s/resize_width';
 
     public function __construct(
         private readonly ScopeConfigInterface $scopeConfig
@@ -23,20 +25,29 @@ class ModuleConfig
         return $this->scopeConfig->getValue(self::XML_PATH_RESIZE_MODE, ScopeInterface::SCOPE_STORE, $scopeId);
     }
 
-    public function getImageId($scopeId = null): ?string
+    public function getResizeImageTypes($scopeId = null): array
     {
-        return $this->scopeConfig->getValue(self::XML_PATH_RESIZE_IMAGE_ID, ScopeInterface::SCOPE_STORE, $scopeId);
+        $resizeImageTypes = $this->scopeConfig->getValue(self::XML_PATH_ACTIVE_IMAGE_TYPES, ScopeInterface::SCOPE_STORE, $scopeId) ?? 'none';
+        return $resizeImageTypesArray = explode(',', $resizeImageTypes);
     }
 
-    public function getResizeHeight($scopeId = null): ?int
+    public function getImageId($imageType, $scopeId = null): ?string
     {
-        return (int)$this->scopeConfig->getValue(self::XML_PATH_RESIZE_HEIGHT, ScopeInterface::SCOPE_STORE, $scopeId)
+        $configPath = sprintf(self::XML_PATH_RESIZE_IMAGE_ID, $imageType);
+        return $this->scopeConfig->getValue($configPath, ScopeInterface::SCOPE_STORE, $scopeId);
+    }
+
+    public function getResizeHeight($imageType, $scopeId = null): ?int
+    {
+        $configPath = sprintf(self::XML_PATH_RESIZE_HEIGHT, $imageType);
+        return (int)$this->scopeConfig->getValue($configPath, ScopeInterface::SCOPE_STORE, $scopeId)
             ?? null;
     }
 
-    public function getResizeWidth($scopeId = null): ?int
+    public function getResizeWidth($imageType, $scopeId = null): ?int
     {
-        return (int)$this->scopeConfig->getValue(self::XML_PATH_RESIZE_WIDTH, ScopeInterface::SCOPE_STORE, $scopeId)
+        $configPath = sprintf(self::XML_PATH_RESIZE_WIDTH, $imageType);
+        return (int)$this->scopeConfig->getValue($configPath, ScopeInterface::SCOPE_STORE, $scopeId)
             ?? null;
     }
 }
